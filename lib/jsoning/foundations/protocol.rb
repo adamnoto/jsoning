c5b78bc20141b0ec683ccfcebd7d20f79a94cf77
Adam Pahlevi <adam.pahlevi@gmail.com>
2015-10-03 08:50:15 +0700

    change method parse signature, and rename it. and add construct_hash_from routine.

--- lib/jsoning/foundations/protocol.rb
+++ lib/jsoning/foundations/protocol.rb
@@ -31,7 +31,7 @@ class Jsoning::Protocol
     pretty = options["pretty"] if pretty.nil?
     pretty = false if pretty.nil?
 
-    data = parse(object)
+    data = retrieve_values_from(object)
 
     if pretty
       JSON.pretty_generate(data)
@@ -40,13 +40,41 @@ class Jsoning::Protocol
     end
   end
 
-  def parse(object)
+  # construct the JSON from given object
+  def retrieve_values_from(object)
     # hold data here
     data = {}
 
     mappers_order.each do |mapper_sym|
       mapper = mapper_for(mapper_sym)
       mapper.extract(object, data)
+    end
+
+    data
+  end
+
+  # construct hash from given JSON
+  def construct_hash_from(json_string)
+    data = {}
+
+    # make all json obj keys to downcase, symbol
+    json_obj = JSON.parse(json_string)
+    json_obj = Hash[json_obj.map { |k, v| [k.to_s.downcase, v]}]
+
+    mappers_order.each do |mapper_sym|
+      mapper = mapper_for(mapper_sym)
+      mapper_key_name = mapper.name.to_s.downcase
+      
+      mapper_default_value = mapper.default_value
+      mapper_key_value = json_obj[mapper_key_name]
+      # retrieve value from key, if not available, from default
+      value = mapper_key_value || mapper_default_value
+
+      if value.nil? && !mapper.nullable?
+        raise Jsoning::Error, "Constructing hash failed due to #{mapper_key_name} being nil when it is not allowed to" 
+      end
+
+      data[mapper_key_name] = value
     end
 
     data
